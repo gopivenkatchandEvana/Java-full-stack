2609607435ba033d4ce36f847b61e9423097df6f
gopivenkatchandEvana <gopichanduevana@gmail.com>
2025-06-18 14:41:54 +0530

    Create Ariarthmetic.java

---
+++ Ariarthmetic.java
@@ -0,0 +1,15 @@
+public class AriarthmeticExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("Addition       : " + (a + b));  // 13
+        System.out.println("Subtraction    : " + (a - b));  // 7
+        System.out.println("Multiplication : " + (a * b));  // 30
+        System.out.println("Division       : " + (a / b));  // 3 (Integer division)
+        System.out.println("Modulus        : " + (a % b));  // 1
+    }
+}
