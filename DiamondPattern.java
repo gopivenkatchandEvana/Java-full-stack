546ac31ab28db93ea84ee551f2c023889148e702
gopivenkatchandEvana <gopichanduevana@gmail.com>
2025-06-18 12:25:17 +0530

    Create DiamondPattern.java

---
+++ DiamondPattern.java
@@ -0,0 +1,31 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 5; // You can change this for a bigger diamond
+
+        // Upper half of diamond (including middle row)
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
