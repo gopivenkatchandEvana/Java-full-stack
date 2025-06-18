28741d819d75d09387a0491bc1d84749efb0edd4
gopivenkatchandEvana <gopichanduevana@gmail.com>
2025-06-18 12:26:42 +0530

    Create HollowDiamond.java

---
+++ HollowDiamond.java
@@ -0,0 +1,43 @@
+public class HollowDiamond {
+    public static void main(String[] args) {
+        int n = 5; // Half of the diamond height
+
+        // Upper Half
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars and spaces in hollow pattern
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        // Lower Half
+        for (int i = n - 1; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars and spaces in hollow pattern
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
